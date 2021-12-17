95a5ac4a10625d29f31282b9b8680220648352e5
Adam Lord <adam@tivix.com>
2021-12-17 14:01:21 -0800

    setup/install requirements

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className="text-6xl font-extrabold text-purple-700">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
