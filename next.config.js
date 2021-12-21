b951439f5740cbd95890c34f9fa795a712ac0bb3
Adam Lord <adam@tivix.com>
2021-12-20 16:12:10 -0800

    VERCEL_URL as Auth0 base

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ module.exports = {
   images: {
     domains: ['s.gravatar.com', 'lh3.googleusercontent.com'],
   },
+  env: {
+    AUTH0_BASE_URL: process.env.VERCEL_URL || process.env.AUTH0_BASE_URL,
+  },
 }
