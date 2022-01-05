12a9c3821dca55b469a2b2262ca3c14cc1690231
Adam Lord <adam@tivix.com>
2022-01-04 17:00:28 -0800

    real basic prisma/db integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,9 @@ import { useUser } from '@auth0/nextjs-auth0';
 import PostPreview from '../components/post-preview';
 import {client} from '../content/contentful';
 import { GetStaticProps } from 'next';
+import { PrismaClient } from '@prisma/client';
 
-export default function Home({ posts }) {
+export default function Home({ posts, users }) {
 
   const { user, error, isLoading } = useUser();
 
@@ -31,6 +32,14 @@ export default function Home({ posts }) {
             ))}
           </div>
         </section>
+        <section className="mt-12">
+          <h2 className="text-3xl font-extrabold text-red-700 mb-6">Users from the Database</h2>
+          <div className="grid gap-6 lg:grid-cols-4">
+            {users.map((user) => (
+              <h1 key={user.id}><span className="font-mono">{user.id}</span> {user.name} <span className="text-red-600">{user.email}</span></h1>
+            ))}
+          </div>
+          </section>
       </main>
     </div>
   )
@@ -42,9 +51,13 @@ export const getStaticProps: GetStaticProps = async() => {
     content_type: 'post'
   })
 
+  const prisma = new PrismaClient()
+  const users = await prisma.user.findMany()
+
   return {
     props: {
-      posts: res.items
+      posts: res.items,
+      users
     }
   }
 }
